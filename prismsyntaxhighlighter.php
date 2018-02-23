a7c5a5611d883c4f7242d066ac16cb6b96e6edfa
Andre Hotzler <git2018@nspw.andrehotzler.de>
2018-02-23 15:55:35 +0100

    First commit

---
+++ prismsyntaxhighlighter.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  Content.prismsyntaxhighlighte
+ *
+ * @copyright   Copyright (C) 2018 Andre Hotzler. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+class plgContentprismsyntaxhighlighter extends JPlugin
+	{
+		public function onContentPrepare($context, &$article, &$params, $page = 0)
+			{
+				$regex = '/<pre class=".*language-.*>/i';
+				preg_match_all($regex, $article->text, $matches, PREG_SET_ORDER);
+				if ($matches)
+					{
+						$app = JFactory::getApplication();
+						if ($app->isSite())
+							{
+								$document = JFactory::getDocument();
+								$document->addStyleSheet(JURI::base($pathonly=true).'/media/plg_content_prismsyntaxhighlighter/css/prism-' . $this->params->def('prismstyle','tinymce') .'.css');
+								$document->addScript(JURI::base($pathonly=true).'/media/plg_content_prismsyntaxhighlighter/js/prism-' . $this->params->def('prismtype','default') . '.js');
+								if ($this->params->def('prismloadlinenumbers','1')) {
+									$document->addStyleSheet(JURI::base($pathonly=true).'/media/plg_content_prismsyntaxhighlighter/css/prism-linenumbers.css');
+								}
+							}
+					}
+			}
+	}
+?>
+
+
